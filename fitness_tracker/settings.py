be30b9713310a88995d97da867b19ad1eadf9ddc
reza khaleghi <rezakhaleghi998@gamil.com>
2025-09-06 22:30:18 +0200

    Fix static files storage

--- fitness_tracker/settings.py
+++ fitness_tracker/settings.py
@@ -192,7 +192,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Use simple static files storage for deployment
+if not DEBUG:
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
